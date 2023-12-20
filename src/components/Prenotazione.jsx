4c12c7e2160495330bcfaddecccaef82a96768fc
gabrieledelfrate <leledelfrate@outlook.com>
2023-12-20 12:37:10 +0100

    aggiornato prenotazione.jsx

--- src/components/Prenotazione.jsx
+++ src/components/Prenotazione.jsx
@@ -68,22 +68,59 @@ class Prenotazione extends React.Component {
 		}
 	};
 
+	/* 	handleSubmit = e => {
+        e.preventDefault();
+		fetch('https://striveschool-api.herokuapp.com/api/reservation', {
+			method: 'POST',
+			body: JSON.stringify(this.state.prenotazione),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(res => {
+				console.log('Risposta', res);
+				if (res.ok) {
+					this.setState({
+						prenotazione: {
+							name: '',
+							phone: '',
+							numberOfPeople: 1,
+							smoking: false,
+							dateTime: '',
+							specialRequests: '',
+						},
+						showAlert: true,
+					});
+				} else {
+					throw new Error(
+						'Errore nel salvataggio della prenotazione'
+					);
+				}
+			})
+			.catch(err => {
+				console.log('Errore:', err);
+			});
+	}; */
+
 	render() {
 		return (
 			<Container>
 				<Row className="justify-content-center mt-3">
 					<Col md={6}>
 						<h2 className="text-center">Modulo di prenotazione</h2>
-						{/* {this.state.showAlert === true && (
+                        {/* Con l'operatore SHORT CIRCUIT (&&) l'alert non viene inserito nel DOM finché showAlert è false */}
+						{this.state.showAlert === true && (
                             <Alert variant="info">Prenotazione salvata!</Alert>
-                        )} */}
-						<Alert
+                        )}
+
+                        {/* Con il ternario l'alert è comunque presente nel DOM con classe d-none, per cui non viene mostrato finché showAlert è false */}
+						{/* <Alert
 							variant="info"
 							className={
 								this.state.showAlert ? 'd-block' : 'd-none'
 							}>
 							Prenotazione salvata!
-						</Alert>
+						</Alert> */}
 
 						<Form onSubmit={this.handleSubmit}>
 							<Form.Group className="mb-3">
@@ -125,7 +162,9 @@ class Prenotazione extends React.Component {
 								<Form.Select
 									aria-label="Quantità"
 									required
-									value={this.state.prenotazione.numberOfPeople}
+									value={
+										this.state.prenotazione.numberOfPeople
+									}
 									onChange={e => {
 										this.handleInputChange(
 											'numberOfPeople',
@@ -142,25 +181,57 @@ class Prenotazione extends React.Component {
 									<option>8</option>
 								</Form.Select>
 							</Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Check type="checkbox" label="Tavolo fumatori" checked={this.state.prenotazione.smoking} onChange={e => {
-                                    this.handleInputChange('smoking', e.target.checked)
-                                    // Le checkbox restituiscono un booleano
-                                }} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Data prenotazione</Form.Label>
-                                <Form.Control type="datetime-local" required value={this.state.prenotazione.dateTime} onChange={e => {
-                                    this.handleInputChange('dateTime', e.target.value)
-                                }} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Note particolari &#40;allergie, intolleranze...&#41;</Form.Label>
-                                <Form.Control as="textarea" rows={3} placeholder="allergie, intolleranze..." value={this.state.prenotazione.specialRequests} onChange={e => {
-                                    this.handleInputChange('specialRequests', e.target.value)
-                                }} />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">Prenota!</Button>
+							<Form.Group className="mb-3">
+								<Form.Check
+									type="checkbox"
+									label="Tavolo fumatori"
+									checked={this.state.prenotazione.smoking}
+									onChange={e => {
+										this.handleInputChange(
+											'smoking',
+											e.target.checked
+										);
+										// Le checkbox restituiscono un booleano
+									}}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>Data prenotazione</Form.Label>
+								<Form.Control
+									type="datetime-local"
+									required
+									value={this.state.prenotazione.dateTime}
+									onChange={e => {
+										this.handleInputChange(
+											'dateTime',
+											e.target.value
+										);
+									}}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>
+									Note particolari &#40;allergie,
+									intolleranze...&#41;
+								</Form.Label>
+								<Form.Control
+									as="textarea"
+									rows={3}
+									placeholder="allergie, intolleranze..."
+									value={
+										this.state.prenotazione.specialRequests
+									}
+									onChange={e => {
+										this.handleInputChange(
+											'specialRequests',
+											e.target.value
+										);
+									}}
+								/>
+							</Form.Group>
+							<Button variant="primary" type="submit">
+								Prenota!
+							</Button>
 						</Form>
 					</Col>
 				</Row>
