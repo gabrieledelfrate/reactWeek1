dc385ead050722785ab933c88d681cec10559618
gabrieledelfrate <leledelfrate@outlook.com>
2023-12-20 11:58:29 +0100

    form prenotazione funzionante

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CustomNavbar from './components/CustomNavbar'
+import CustomNavbar from './components/CustomNavbar';
 import Home from './components/Home';
+import Prenotazione from './components/Prenotazione';
 
 
 function App() {
-  return (
-    <div>
-      <CustomNavbar testoAddizionale="Il nostro risorante epico!"/> 
-      <Home />
-     
-    </div>
-  );
+	return (
+		<div>
+			<CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
+			<Home />
+			<Prenotazione />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Prenotazione.jsx
+++ src/components/Prenotazione.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Row, Col, Container, Form, Button, Alert } from 'react-bootstrap';
 
 /* Quali campi usiamo per la prenotazione?
-1- nome: string obbligatorio
-2- telefono: string obbligatorio
-3- persone: number valore predefinito: 1
-4- fumatori: boolean
+1- name: string obbligatorio
+2- phone: string obbligatorio
+3- numberOfPeople: number valore predefinito: 1
+4- smoking: boolean
 5- data e ora: string obbligatorio
-6- note: string
+6- specialRequests: string
 */
 
 class Prenotazione extends React.Component {
@@ -15,12 +15,12 @@ class Prenotazione extends React.Component {
 		super(props);
 		this.state = {
 			prenotazione: {
-				nome: '',
-				telefono: '',
-				persone: 1,
-				fumatori: false,
-				dataOra: '',
-				note: '',
+				name: '',
+				phone: '',
+				numberOfPeople: 1,
+				smoking: false,
+				dateTime: '',
+				specialRequests: '',
 			},
 			showAlert: false,
 		};
@@ -30,11 +30,11 @@ class Prenotazione extends React.Component {
 		this.setState({
 			prenotazione: { ...this.state.prenotazione, [proprieta]: valore },
 		});
-		// Se vogliamo utilizzare un parametro o una variabile come NOME di una proprietà di un oggetto dobbiamo valutare il contenuto del parametro o della variabile con le quadre []. In questo caso proprieta può essere nome, telefono, persone...
+		// Se vogliamo utilizzare un parametro o una variabile come NOME di una proprietà di un oggetto dobbiamo valutare il contenuto del parametro o della variabile con le quadre []. In questo caso proprieta può essere name, phone, numberOfPeople...
 	};
 
 	handleSubmit = async e => {
-		e.preventDefaut();
+		e.preventDefault();
 		console.log('Invio prenotazione');
 		// Fetch con metodo POST
 		try {
@@ -51,12 +51,12 @@ class Prenotazione extends React.Component {
 			if (res.ok) {
 				this.setState({
 					prenotazione: {
-						nome: '',
-						telefono: '',
-						persone: 1,
-						fumatori: false,
-						dataOra: '',
-						note: '',
+						name: '',
+						phone: '',
+						numberOfPeople: 1,
+						smoking: false,
+						dateTime: '',
+						specialRequests: '',
 					},
 					showAlert: true,
 				});
@@ -84,6 +84,84 @@ class Prenotazione extends React.Component {
 							}>
 							Prenotazione salvata!
 						</Alert>
+
+						<Form onSubmit={this.handleSubmit}>
+							<Form.Group className="mb-3">
+								<Form.Label>Il tuo nome</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="nome e cognome"
+									required
+									value={this.state.prenotazione.name}
+									onChange={e => {
+										this.setState({
+											prenotazione: {
+												...this.state.prenotazione,
+												name: e.target.value,
+											},
+											// potremmo avere la tentazione di fare this.state.prenotazione.name = e.target.value, ma è VIETATISSIMO perché lo state è di sola lettura!
+										});
+									}}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>Recapito telefonico</Form.Label>
+								<Form.Control
+									type="tel"
+									placeholder="xxxxxxxxxx"
+									required
+									value={this.state.prenotazione.phone}
+									onChange={e => {
+										this.handleInputChange(
+											'phone',
+											e.target.value
+										);
+										// Visto che tutti gli onChange fanno la stessa cosa, cioè settano una proprietà dell'oggetto nello state, chiamiamo il metodo handleInputChange (riga 29) passando la proprietà che deve essere settata, letta dal metodo tra parentesi quadre, e il valore
+									}}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>Numero di coperti</Form.Label>
+								<Form.Select
+									aria-label="Quantità"
+									required
+									value={this.state.prenotazione.numberOfPeople}
+									onChange={e => {
+										this.handleInputChange(
+											'numberOfPeople',
+											e.target.value
+										);
+									}}>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+									<option>6</option>
+									<option>7</option>
+									<option>8</option>
+								</Form.Select>
+							</Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Check type="checkbox" label="Tavolo fumatori" checked={this.state.prenotazione.smoking} onChange={e => {
+                                    this.handleInputChange('smoking', e.target.checked)
+                                    // Le checkbox restituiscono un booleano
+                                }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Data prenotazione</Form.Label>
+                                <Form.Control type="datetime-local" required value={this.state.prenotazione.dateTime} onChange={e => {
+                                    this.handleInputChange('dateTime', e.target.value)
+                                }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Note particolari &#40;allergie, intolleranze...&#41;</Form.Label>
+                                <Form.Control as="textarea" rows={3} placeholder="allergie, intolleranze..." value={this.state.prenotazione.specialRequests} onChange={e => {
+                                    this.handleInputChange('specialRequests', e.target.value)
+                                }} />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">Prenota!</Button>
+						</Form>
 					</Col>
 				</Row>
 			</Container>
